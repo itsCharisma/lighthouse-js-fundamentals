f2a02ed3e000dc6157263eecbb2352d423bb7c22
Devon Blake <devon.blake@live.com>
2019-07-12 22:02:30 +0000

    sayHello function

---
+++ hello.js
@@ -0,0 +1,14 @@
+/*const sayHello  = function () {
+console.log("Hello, world");
+}
+
+sayHello();
+*/
+
+const sayHello  = function (name) {
+console.log("Hello, " + name);
+}
+
+sayHello("Devon");
+sayHello("Evelyn");
+sayHello("Jaimie");
