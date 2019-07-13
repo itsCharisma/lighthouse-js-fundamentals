ccddd9f041f02cdde615b0ef452eecf9d3997239
Devon Blake <devon.blake@live.com>
2019-07-13 01:08:45 +0000

    Final?

---
+++ ageCalculator.js
@@ -0,0 +1,16 @@
+function ageCalculator (name, yearOfBirth, currentYear) {
+  var year = currentYear - yearOfBirth;
+  return name + " is " + year + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+
+
+
+
+
+
+
+
---
+++ ice-cream.js
@@ -0,0 +1,12 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+
+
+
+
+
+
